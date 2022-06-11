44dc589569381a857295cfcec3830b451b72b5a1
6ichem <6ichem.dev@gmail.com>
2022-06-11 11:28:26 +0100

    revert

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   css: [
     "primevue/resources/themes/mdc-dark-deeppurple/theme.css",
     "primevue/resources/primevue.css",
+    "primeicons/primeicons.css",
   ],
   vite: {
     resolve: {
