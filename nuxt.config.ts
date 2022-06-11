29bedbd63d69fd8fdca23e1b37c54e09742903e9
6ichem <6ichem.dev@gmail.com>
2022-06-11 12:55:01 +0100

    update favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,9 +21,7 @@ export default defineNuxtConfig({
       title: "Sell.app Migration",
       viewport: "width=device-width, initial-scale=1, maximum-scale=1",
       charset: "utf-8",
-      link: [
-        { rel: "icon", type: "image/x-icon", href: "./static/favicon.ico" },
-      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
 });
